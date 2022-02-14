6b79277bd5f65597392ee2069f3167c4f7b57a14
ikenna <ikenna.porter@gmail.com>
2022-02-14 19:28:44 +0100

    Finished

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+    
+    let repeatedStr = "";
+    if (num > -1) {
+        for (let i = 0; i < num; i++) {
+            repeatedStr += str;
+        }
+    } else {
+        return "ERROR";
+    }
+    
+    return repeatedStr;
 };
 
 // Do not edit below this line
