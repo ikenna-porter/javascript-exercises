35c9788b9f38b973dd53eadc84f1343a65072d13
ikenna <ikenna.porter@gmail.com>
2022-02-15 22:05:18 +0100

    Finished

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,8 +1,7 @@
 const leapYears = function (year) {
-    if (year % 4 === 0) return true;
-    else if (year % 400 === 0) return true;
-    else return false
-
+    if (year % 4 !== 0) return false;
+    if (year % 100 === 0 && year % 400 !== 0) return false;
+    else return true;
 };
 
 // Do not edit below this line
