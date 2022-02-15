a32ccf4839f671f7b6f23c1afcfb8d2d7cbf04eb
ikenna <ikenna.porter@gmail.com>
2022-02-15 22:33:41 +0100

    Finished

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
+const ftoc = function(temp) {
+  let cel = (temp - 32) * (5/9);
 
+  return Number(cel.toFixed(1));
 };
 
-const ctof = function() {
+const ctof = function(temp) {
+  let fahr = temp * (9/5) + 32;
 
+  return Number(fahr.toFixed(1));
 };
 
 // Do not edit below this line
