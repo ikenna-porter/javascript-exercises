9e3d96e4e53c52e79ab1bc5938cf85cd18daef56
ikenna <ikenna.porter@gmail.com>
2022-04-04 18:27:21 +0200

    Finish'

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,21 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    //declare array
+    const arr = [1, 1];
 
+    //doesn't allow negative numbers
+    if (num < 0) return "OOPS";
+
+    //converts string numbers into numbers
+    num = Number(num);
+
+    //create for loop that increments up to num and pushes i + i-1 to arr
+    for (let i = 1; i < num; i++) {
+        arr.push(arr[i] + arr[i-1]);
+    }
+    return arr[num-1];
 };
+
+fibonacci(10);
 
 // Do not edit below this line
 module.exports = fibonacci;
