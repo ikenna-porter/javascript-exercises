9d3ef7c5d22e2287693527acb2e49bdea53dac07
ikenna <ikenna.porter@gmail.com>
2022-04-04 18:46:31 +0200

    Finished

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,7 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+    bookArr = [];
+    books.forEach( book => bookArr.push(book.title));
+    return bookArr;
 };
 
 // Do not edit below this line
