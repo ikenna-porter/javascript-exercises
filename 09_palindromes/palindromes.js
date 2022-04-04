1efbb658c61263fecf8bbdd9a8e3818311328cb8
ikenna <ikenna.porter@gmail.com>
2022-04-04 18:03:35 +0200

    Finished

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,23 @@
-const palindromes = function () {
+const palindromes = function (word) {
+    
+    //creates palindrome
+    const wordArr = word.toLowerCase().split('').reverse();
+    //removes spaces and special characters from palindrome
+    let newArr = wordArr.filter( letter => {
+        let wordChars = /[a-z]/;
+        return wordChars.test(letter);
+    });
 
+    //removes spaces and special characters from word
+    word = word.toLowerCase().split('').filter( letter => {
+        let wordChars = /[a-z]/;
+        return wordChars.test(letter);
+    })
+
+    console.log(word.join(''));
+    console.log(newArr.join(''));
+    if (newArr.join('') === word.join('')) return true;
+    return false;
 };
 
 // Do not edit below this line
