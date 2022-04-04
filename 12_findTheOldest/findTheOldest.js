2d89dd671b8e5094530224f3a850857b98f3f91a
ikenna <ikenna.porter@gmail.com>
2022-04-04 19:25:47 +0200

    Finished

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,19 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
 
-};
+    //creates label for current year
+    const date = new Date().getFullYear();
+
+    //creates age key in each object
+    people.map(person => {
+        if (!person.yearOfDeath) {
+            return person.age = date - person.yearOfBirth;
+        } else {
+            return person.age = person.yearOfDeath - person.yearOfBirth;
+        }
+    });
+    //reduces people array down to the oldest person
+    return people.reduce((a, b) => a.age > b.age ? a : b);
+}; 
 
 // Do not edit below this line
 module.exports = findTheOldest;
